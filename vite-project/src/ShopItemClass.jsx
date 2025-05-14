d0af3a8a6765bbad367265ff056d320ee52c3f3c
Yuriy Zinchenko <yurazinch@gmail.com>
2025-05-14 11:33:55 +0300

    добавил конструктор - без изменений

--- vite-project/src/ShopItemClass.jsx
+++ vite-project/src/ShopItemClass.jsx
@@ -1,22 +1,25 @@
 import { Component } from "react"
 export class ShopItemClass extends Component {
-     
+     constructor(item) {
+        super(item);
+        this.item = item;
+     }
     render(){
-                
+                const {brand, title, description, descriptionFull, currency, price} = this.item;
         return (
             <div className="main-content">
-                <h2>{this.props.brand}</h2>
-                <h1>{this.props.title}</h1>
-                <h3>{this.props.description}</h3>
+                <h2>{brand}</h2>
+                <h1>{title}</h1>
+                <h3>{description}</h3>
                 <div className="description">
-                    {this.props.descriptionFull}
+                    {descriptionFull}
                 </div>
                 <div className="highlight-window mobile">
                     <div class="highlight-overlay"></div>
                 </div>
                 <div className="divider"></div>
                 <div className="purchase-info">
-                    <div className="price">{this.props.currency}{this.props.price}</div>
+                    <div className="price">{currency}{price}</div>
                     <button>Добавить в корзину</button>
                 </div>
             </div>
